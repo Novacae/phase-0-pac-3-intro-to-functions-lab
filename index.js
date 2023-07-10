2319cbbdec75d7e002bcb75d92de0fb4e8ae2b3c
aman githinji <amangithinji09@gmail.com>
2023-07-10 17:05:37 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string) {
+    return string;
+}
+
+function shout(string) {
+    return string.toUpperCase();
+  }
+  
+function whisper(string) {
+    return string;
+}
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout(string) {
+    spy.call('HELLO');
+}
+console.log(string.toUpperCase());
+
+function logWhisper(string) {  
+    spy.call('hello');
+}
+console.log(string.toLowerCase());
+
+function sayHiToHeadphonedRoommate(string) {
+    if(string === 'hello')
+    return"I can't hear you!"
+      else if(string === 'HELLO')
+    return"YES INDEED!"
+      else(string = "Let's have dinner together!")
+    return"I would love to!"
+}
